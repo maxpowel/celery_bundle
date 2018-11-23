c49d63a1c616cfe881968ceaabb9dc9088af4e06
Álvaro García <agarcia@bmat.com>
2018-11-23 12:19:47 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
 setup(
   name='celery_bundle',
   packages=['celery_bundle'],
-  version='1.18',
+  version='1.19',
   description='Celery support for applauncher',
   author='Alvaro Garcia Gomez',
   author_email='maxpowel@gmail.com',
